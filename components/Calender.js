498e812b948832e067b96f5c8d61847096f7f65a
Utkarsh <Aishwary.009.shrivastava@culturtap.com>
2023-12-31 16:20:56 +0530

    schedules added in the dates with dummy data

--- components/Calender.js
+++ components/Calender.js
@@ -9,7 +9,6 @@ const Calendar = () => {
   const [calendarData, setCalendarData] = useState([]);
 
   useEffect(() => {
-
     console.log(`Fetch data for ${displayedYear}-${displayedMonth + 1}`);
 
     const firstDay = getFirstDayOfMonth(displayedYear, displayedMonth);
@@ -54,23 +53,53 @@ const Calendar = () => {
   };
 
   const handleMonthClick = () => {
-    // You can implement month navigation here
     console.log('Month clicked');
   };
 
   const handleYearClick = () => {
-    // You can implement year navigation here
     console.log('Year clicked');
   };
 
-
-
-const renderCalendar = () => {
-    const currentDay = currentDate.getDate(); // Get the current day
+  const renderCalendar = () => {
+    const currentDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
     const firstDay = getFirstDayOfMonth(displayedYear, displayedMonth);
     const totalDays = getDaysInMonth(displayedYear, displayedMonth);
     const daysArray = Array.from({ length: totalDays }, (_, i) => i + 1);
-  
+
+    const scheduleData = {
+      '2023-12-05': [
+        { time: '10:00 AM', task: 'Batch001' },
+        { time: '02:00 PM', task: 'Batch002' },
+      ],
+      '2023-12-15': [
+        { time: '02:30 PM', task: 'Batch003' },
+        { time: '04:00 PM', task: 'Batch004' },
+      ],
+      '2023-12-20': [
+        { time: '11:00 AM', task: 'Batch005' },
+        { time: '03:30 PM', task: 'Batch006' },
+      ],
+      '2023-12-25': [
+        { time: '09:30 AM', task: 'Batch007' },
+        { time: '01:00 PM', task: 'Batch008' },
+      ],
+      // Add more schedule data as needed
+    };
+
+    const previousMonthDays = getDaysInMonth(
+      displayedMonth === 0 ? displayedYear - 1 : displayedYear,
+      displayedMonth === 0 ? 11 : displayedMonth - 1
+    );
+    const previousMonthStart = previousMonthDays - firstDay + 1;
+    const previousMonthDates = Array.from({ length: firstDay }, (_, i) => previousMonthStart + i);
+
+    const remainingCells = 42 - (previousMonthDates.length + daysArray.length);
+    const nextMonthDates = Array.from({ length: remainingCells }, (_, i) => i + 1);
+
+    const allDates = [...previousMonthDates, ...daysArray, ...nextMonthDates];
+
     return (
       <div className="grid grid-cols-7 gap-1">
         {daysOfWeek.map((day) => (
@@ -78,16 +107,45 @@ const renderCalendar = () => {
             {day}
           </div>
         ))}
-        {calendarData.map((day, index) => (
-          <div
-            key={index}
-            className={`lg:p-10 p-7 ${
-              day === currentDay ? 'bg-blue-500 text-white' : day ? 'bg-gray-200 cursor-pointer' : 'bg-gray-100'
-            } text-black`}
-          >
-            {day}
-          </div>
-        ))}
+        {allDates.map((day, index) => {
+          const isLiveMonth = displayedMonth === currentMonth && displayedYear === currentYear;
+          const currentDateStr = `${displayedYear}-${displayedMonth + 1}-${day}`;
+          const daySchedule = scheduleData[currentDateStr] || [];
+
+          const isBeyondCurrentMonth = day > totalDays;
+
+          const hasFirstTask = daySchedule.length > 0;
+          const hasSecondTask = daySchedule.length > 1;
+
+          return (
+            <div
+              key={index}
+              className={`lg:p-10 p-7 ${
+                day === 1 && !isBeyondCurrentMonth ? 'lg:text-xl text-md font-bold' : ''
+              } ${
+                (day === currentDay && isLiveMonth) ? 'bg-blue-500 text-white' : ''
+              } ${
+                isBeyondCurrentMonth ? 'text-gray-400' : 'bg-gray-200 cursor-pointer'
+              } text-black relative`} // Added 'relative' class to allow absolute positioning of circles
+            >
+              {day === 1 && !isBeyondCurrentMonth ? '1st' : isBeyondCurrentMonth ? '' : day}
+
+
+
+
+              {daySchedule.length > 0 && (
+                <div className="text-sm">
+                  {daySchedule.map((schedule, scheduleIndex) => (
+                    <div key={scheduleIndex}>
+                                    
+                      {schedule.time}: {schedule.task}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
     );
   };
